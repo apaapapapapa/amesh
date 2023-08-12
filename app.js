971ba0d7530b793fb99775dbbd78064d63460af7
apaapapapapa <raha3415kohei@gmail.com>
2023-08-12 12:37:20 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let currentAnswer = "";
+
+const questionData = {
+    factorization: [
+        { "q": "x^2 - 4", "a": "(x + 2)(x - 2)" },
+        { "q": "x^2 - 1", "a": "(x + 1)(x - 1)" }
+    ],
+    expansion: [
+        { "q": "(x + 2)(x - 2)", "a": "x^2 - 4" },
+        { "q": "(x + 1)(x - 1)", "a": "x^2 - 1" }
+    ]
+};
+
+function getQuestions(unit) {
+    return questionData[unit];
+}
+
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateQuestion() {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
+    const units = Array.from(checkboxes).map(cb => cb.value);
+    if (!units.length) {
+        alert("少なくとも1つの単元を選択してください！");
+        return;
+    }
+
+    const unit = getRandomElement(units);
+    const questions = getQuestions(unit);
+    const question = getRandomElement(questions);
+
+    document.getElementById("questionArea").textContent = question.q;
+    currentAnswer = question.a;
+
+    deleteAnswer();
+}
+
+function showAnswer() {
+    document.getElementById("answerArea").textContent = currentAnswer;
+}
+
+function deleteAnswer() {
+    document.getElementById("answerArea").textContent = "";
+}
